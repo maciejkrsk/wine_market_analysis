fa184e6e586ab089d1164f8eebe259c3f9b6ec83
maciej_desktop <maciej.krsk@gmail.com>
2023-06-15 17:03:14 +0200

    small prog on streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,55 @@ import streamlit as st
 
 st.title("Vivino market analysis report")
 st.subheader("Introduction")
-st.markdown('We have been asked to create a report of Vivino\'s market analysis, in order to do that, we have been asked to gather some market information with some tasks'
-            )
+st.markdown("""We have been asked to create a report of Vivino\'s market analysis, in order to do that, we have been asked to gather some market information with some tasks
+            """)
 
 st.subheader('What are the wines to highlight to increase our sales?')
 
 
 st.subheader('Top 5 recommendations for Cabernet Sauvignon enjoyers')
 st.subheader('Recommendations to improve the data')
+
+
+#Sidebar
+st.sidebar.header('Summary')
+st.sidebar.markdown(
+    """So right here I used a markdown with 3 \" 
+    [Introduction](#Recommendations-to-improve-the-data)
+    """
+)
+st.sidebar.markdown(
+    """ 
+    [What country to prioritize](#Recommendations-to-improve-the-data)
+    """
+)
+st.sidebar.markdown(
+    """
+    [Best winery prize](#Recommendations-to-improve-the-data)
+    """
+)
+st.sidebar.markdown(
+    """
+    [Tastes combination](#Recommendations-to-improve-the-data)
+    """
+)
+st.sidebar.markdown(
+    """ 
+    [Most common grapes](#Recommendations-to-improve-the-data)
+    """
+)
+st.sidebar.markdown(
+    """ 
+    [Country leaderboards](#Recommendations-to-improve-the-data)
+    """
+)
+st.sidebar.markdown(
+    """ 
+    [Wines recommended](#Recommendations-to-improve-the-data)
+    """
+)
+st.sidebar.markdown(
+    """ 
+    [General recommendations](#Recommendations-to-improve-the-data)
+    """
+)
